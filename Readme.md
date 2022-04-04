7f9af8ab3965dd5c56c621871b344711beedeb52
Patrick <patrickpqdt87289@gmail.com>
2022-04-04 11:44:03 -0300

    connected-with-database-mongodb

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Gateway Payment
+
+### ☄️ Descrição ☄️
+<p>
+    gateway payment with NodeJS and ReactJS
+</p>
+
+### 🌌 Tecnologias 🌌
+
+- ReactJs
+- NodeJS
+
+## Database
+
+- MongoDB Atlas
+
+## 🚀 Usage
+
+```sh
+yarn start
+
+```
+
+## Author
+
+[<img src="https://avatars.githubusercontent.com/u/69186374?v=4" width="75px;"/>](https://github.com/setxpro)
+
+[Patrick Anjos](eclectic-axolotl-babdb2.netlify.app/)
--- src/Controllers/CartController.js
+++ src/Controllers/CartController.js
@@ -1,7 +1,29 @@
-
+import Cart from "../Models/Cart";
 class CartController {
     async index(req, res) {
-        return res.status(200).json({foo: 'bar'})
+
+        try {
+            const carts = await Cart.find();
+            return res.status(200).json(carts);
+            
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({error: "Internal server error"});
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const { code, price } = req.body;
+
+            const cart = await Cart.create({ code, price });
+
+            return res.status(201).json(cart);
+
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({error: "Internal server error"});
+        }
     }
 }
 
